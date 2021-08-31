0c0003a8cd17cc33600282bc36c2a54cb6ccc787
Gal Zahavi <galzahavi@google.com>
2021-08-31 12:24:23 -0700

    lint fix

--- samples/test/page-management.test.js
+++ samples/test/page-management.test.js
@@ -30,7 +30,8 @@ describe('should test page management functions', async () => {
 
   it('should create a page', async () => {
     const cmd = 'node create-page.js';
-    const output = exec(`${cmd} ${projectId} ${agentID} ${flowId} ${location} ${pageName}`);
+    const output = exec(`${cmd} ${projectId} ${agentID} 
+        ${flowId} ${location} ${pageName}`);
     assert.include(output, pageName);
   });
 
@@ -66,7 +67,8 @@ describe('should test page management functions', async () => {
     }
 
     const cmd = 'node delete-page.js';
-    const output = exec(`${cmd} ${projectId} ${agentID} ${flowId} ${pageID} global`);
+    const output = exec(`${cmd} ${projectId} ${agentID} 
+        ${flowId} ${pageID} global`);
     assert.equal(output, '[ {}, null, null ]\n');
   });
 });
