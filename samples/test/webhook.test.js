51576ef0d43e84e0c7bbfd24411c7db5c623bdbf
Gal Zahavi <galzahavi@google.com>
2021-09-15 13:53:17 -0700

    lint fix

--- samples/test/webhook.test.js
+++ samples/test/webhook.test.js
@@ -14,45 +14,47 @@
 
 'use strict';
 
-const { assert } = require('chai');
-const { describe, it } = require('mocha');
+const {assert} = require('chai');
+const {describe, it} = require('mocha');
 const webhook = require('../webhooks');
 
 const request = {
-    "body": {
-        "detectIntentResponseId": "0a190188-37e9-400a-89e3-f3e878c86916",
-        "intentInfo": {
-            "lastMatchedIntent": "projects/galstarter-316823/locations/us-central1/agents/0078f973-e212-4c69-a707-a6e66e3270ce/intents/00000000-0000-0000-0000-000000000000",
-            "displayName": "Default Welcome Intent",
-            "confidence": 1.0
-        },
-        "pageInfo": {
-            "currentPage": "projects/galstarter-316823/locations/us-central1/agents/0078f973-e212-4c69-a707-a6e66e3270ce/flows/00000000-0000-0000-0000-000000000000/pages/START_PAGE"
-        },
-        "sessionInfo": {
-            "session": "projects/galstarter-316823/locations/us-central1/agents/0078f973-e212-4c69-a707-a6e66e3270ce/sessions/bd38c7-398-d25-963-7c23a0533"
-        },
-        "fulfillmentInfo": {
-            "tag": "Default Welcome Intent"
-        },
-        "text": "hi",
-        "languageCode": "en"
+  body: {
+    detectIntentResponseId: '0a190188-37e9-400a-89e3-f3e878c86916',
+    intentInfo: {
+      lastMatchedIntent:
+        'projects/galstarter-316823/locations/us-central1/agents/0078f973-e212-4c69-a707-a6e66e3270ce/intents/00000000-0000-0000-0000-000000000000',
+      displayName: 'Default Welcome Intent',
+      confidence: 1.0,
     },
-}
-
+    pageInfo: {
+      currentPage:
+        'projects/galstarter-316823/locations/us-central1/agents/0078f973-e212-4c69-a707-a6e66e3270ce/flows/00000000-0000-0000-0000-000000000000/pages/START_PAGE',
+    },
+    sessionInfo: {
+      session:
+        'projects/galstarter-316823/locations/us-central1/agents/0078f973-e212-4c69-a707-a6e66e3270ce/sessions/bd38c7-398-d25-963-7c23a0533',
+    },
+    fulfillmentInfo: {
+      tag: 'Default Welcome Intent',
+    },
+    text: 'hi',
+    languageCode: 'en',
+  },
+};
 
 describe('create agent', () => {
+  it('should test webhook returns correct response', async () => {
+    const temp = JSON.stringify(request);
+    let response = '';
 
-    it('should test webhook returns correct response', async () => {
-        const temp = JSON.stringify(request)
-        let response = ""
+    const res = {
+      send: function (s) {
+        response = JSON.stringify(s);
+      },
+    };
 
-        const res = {
-            status: function(s){},
-            send: function(s){response = JSON.stringify(s)}
-        };
-    
-        webhook.handleWebhook(JSON.parse(temp), res);
-        assert.include(response, "Hello from a GCF Webhook");
-    });
+    webhook.handleWebhook(JSON.parse(temp), res);
+    assert.include(response, 'Hello from a GCF Webhook');
+  });
 });
