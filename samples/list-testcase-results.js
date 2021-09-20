a1be8a18625bc79af1616357ff9de1a74afba458
Gal Zahavi <galzahavi@google.com>
2021-09-20 13:07:15 -0700

    docs: add filter code sample

---
+++ samples/list-testcase-results.js
@@ -0,0 +1,48 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+async function main(project_id, agent_id, test_id, location) {
+  const parent =
+    'projects/' +
+    project_id +
+    '/locations/' +
+    location +
+    '/agents/' +
+    agent_id +
+    '/testCases/' +
+    test_id;
+
+  const {TestCasesClient, protos} = require('@google-cloud/dialogflow-cx');
+
+  const client = new TestCasesClient({
+    apiEndpoint: 'global-dialogflow.googleapis.com',
+  });
+  const req =
+    new protos.google.cloud.dialogflow.cx.v3.ListTestCaseResultsRequest();
+
+  req.parent = parent;
+  req.filter = "environment=draft"
+
+  const res =await client.listTestCaseResults(req);
+
+  console.log(res);
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+main(...process.argv.slice(2));
---
+++ samples/test/list_testcase-results.test.js
@@ -0,0 +1,34 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {assert} = require('chai');
+const {describe, it} = require('mocha');
+const execSync = require('child_process').execSync;
+const exec = cmd => execSync(cmd, {encoding: 'utf8'});
+const dialogflow = require('@google-cloud/dialogflow-cx');
+
+describe('create agent', () => {
+  const cmd = 'node list-testcase-results.js';
+  const agentId = 'be66a5a2-4602-4f8c-a3f3-046896904362'
+  const testId = '9ebddb66-c49e-455b-bacc-24c0538ece01'
+  const agentClient = new dialogflow.AgentsClient();
+  const projectId = await agentClient.getProjectId();
+
+  it('should create agent', async () => {
+    const output = exec(`${cmd} ${projectId} ${agentId} ${testId} ${location}`);
+    assert.include(output, testId);
+  });
+});
