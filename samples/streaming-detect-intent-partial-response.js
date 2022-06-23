a8090a9003863c3a82227a6e08f14b3cab0cedf9
aribray <45905583+aribray@users.noreply.github.com>
2022-06-22 23:20:14 -0500

    add streamingDetectIntent with partial response enabled sample and test
    Change-Id: I499e395658d8c8b0e6f7f2229bb70d1969b25b9b

---
+++ samples/streaming-detect-intent-partial-response.js
@@ -0,0 +1,166 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+async function main(
+  projectId,
+  location,
+  agentId,
+  audioFileName,
+  encoding,
+  sampleRateHertz
+) {
+  // [START dialogflow_cx_streaming_detect_intent_enable_partial_response]
+  /**
+   * TODO(developer): Uncomment these variables before running the sample.
+   */
+  // const projectId = 'my-project';
+  // const location = 'global';
+  // const agentId = 'my-agent';
+  // const audioFileName = '/path/to/audio.raw';
+  // const encoding = 'AUDIO_ENCODING_LINEAR_16';
+  // const sampleRateHertz = 16000;
+  // const languageCode = 'en-US'
+
+  // Imports the Google Cloud Some API library
+  const {SessionsClient} = require('@google-cloud/dialogflow-cx');
+  /**
+   * Example for regional endpoint:
+   *   const location = 'us-central1'
+   *   const client = new SessionsClient({apiEndpoint: 'us-central1-dialogflow.googleapis.com'})
+   */
+  const client = new SessionsClient();
+
+  const fs = require('fs');
+  const util = require('util');
+  const {Transform, pipeline} = require('stream');
+  const pump = util.promisify(pipeline);
+
+  async function streamingDetectIntentPartialResponse() {
+    const sessionId = Math.random().toString(36).substring(7);
+    const sessionPath = client.projectLocationAgentSessionPath(
+      projectId,
+      location,
+      agentId,
+      sessionId
+    );
+
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        audio: {
+          config: {
+            audio_encoding: encoding,
+            sampleRateHertz: sampleRateHertz,
+            singleUtterance: true,
+          },
+        },
+        languageCode: 'en-US',
+      },
+      enablePartialResponse: true,
+    };
+
+    const stream = await client.streamingDetectIntent();
+    stream.on('data', data => {
+      if (data.detectIntentResponse) {
+        const result = data.detectIntentResponse.queryResult;
+
+        for (const message of result.responseMessages) {
+          if (message.text) {
+            console.log(`Agent Response: ${message.text.text}`);
+          }
+        }
+      }
+    });
+    stream.on('error', err => {
+      console.log(err);
+    });
+    stream.on('end', () => {
+      /* API call completed */
+    });
+
+    stream.write(request);
+
+    // Stream the audio from audio file to Dialogflow.
+    await pump(
+      fs.createReadStream(audioFileName),
+      // Format the audio stream into the request format.
+      new Transform({
+        objectMode: true,
+        transform: (obj, _, next) => {
+          next(null, {queryInput: {audio: {audio: obj}}});
+        },
+      }),
+      stream
+    );
+
+    stream.end();
+
+    // [END dialogflow_cx_streaming_detect_intent_enable_partial_response]
+  }
+  streamingDetectIntentPartialResponse();
+}
+main(...process.argv.slice(2));
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+
+// Create a stream for the streaming request.
+// const detectStream = client
+//   .streamingDetectIntent()
+//   .on('error', console.error)
+//   .on('data', data => {
+//     if (data.recognitionResult) {
+//       console.log(
+//         `Intermediate Transcript: ${data.recognitionResult.transcript}`
+//       );
+//     } else {
+//       console.log('Detected Intent:');
+//       const result = data.detectIntentResponse.queryResult;
+//       console.log('RESPONSE TYPE', data.detectIntentResponse.responseType);
+
+//       console.log(`User Query: ${result.transcript}`);
+//       for (const message of result.responseMessages) {
+//         if (message.text) {
+//           console.log(`Agent Response: ${message.text.text}`);
+//         }
+//       }
+//       if (result.match.intent) {
+//         console.log(`Matched Intent: ${result.match.intent.displayName}`);
+//       }
+//       console.log(`Current Page: ${result.currentPage.displayName}`);
+//     }
+//   });
+
+// Write the initial stream request to config for audio input.
+// const initialStreamRequest = {
+//   session: sessionPath,
+//   queryInput: {
+//     audio: {
+//       config: {
+//         audioEncoding: encoding,
+//         sampleRateHertz: sampleRateHertz,
+//         singleUtterance: true,
+//       },
+//     },
+//     languageCode: languageCode,
+//   },
+//   enablePartialResponse: true,
+// };
+// detectStream.write(initialStreamRequest);
+
+// detectIntentAudio();
+// }
